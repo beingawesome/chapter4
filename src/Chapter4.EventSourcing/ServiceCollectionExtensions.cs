59d5ebb024e0febdd83a22366c15727397d10ade
Adam Balicki <awesome@beingaweso.me>
2017-11-03 13:32:00 -0700

    Removed unnecessary parameter

--- src/Chapter4.EventSourcing/ServiceCollectionExtensions.cs
+++ src/Chapter4.EventSourcing/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ namespace Chapter4.EventSourcing
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddEventSourcing(this IServiceCollection services, Uri uri)
+        public static IServiceCollection AddEventSourcing(this IServiceCollection services)
         {
             services.AddTransient<AggregateRepository>();
 
