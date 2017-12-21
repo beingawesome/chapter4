e1e42d72d7c41c80931d79f418b3a00d6619dfa1
Adam Balicki <beingawesome@users.noreply.github.com>
2017-12-21 13:41:49 -0600

    FIXED: IHostedService registration

---
+++ publish.ps1
@@ -0,0 +1,23 @@
+param([string]$key, [string]$vnext)
+
+Get-ChildItem . -recurse -Filter *.csproj | 
+Foreach-Object {
+    $file = $_.FullName
+
+    Get-Content $file | % { $_ -replace "<Version>(.+)</Version>", "<Version>$vnext</Version>" } | Set-Content -Path $file
+}
+
+$root = Get-Location
+$publish = Join-Path -Path $root -ChildPath ".publish"
+
+Write-Host $publish
+
+Remove-Item $publish -Force -Recurse -ErrorAction Ignore
+
+iex "dotnet pack -c Release -o $publish"
+
+Set-Location -Path $publish
+
+#Get-ChildItem . -recurse -Filter *.nupkg | Write-Host
+
+iex "dotnet nuget push *.nupkg -k $key -s https://api.nuget.org/v3/index.json"
